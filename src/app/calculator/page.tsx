33a9fab09f9a5b56e63966f8983b628c67175f81
Pablo Aravena <paravena74@gmail.com>
2023-05-28 19:34:27 -0400

    adding public routes

---
+++ src/app/calculator/page.tsx
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+import { UserButton } from '@clerk/nextjs';
+import Header from '@/components/header/Header';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return <main>Calculator private</main>;
+}
