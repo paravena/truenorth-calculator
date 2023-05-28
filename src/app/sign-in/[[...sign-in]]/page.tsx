3e556f3d3e9f58f22e4d52fdf9ca91be9c42f65e
Pablo Aravena <paravena74@gmail.com>
2023-05-28 13:48:05 -0400

    some debugging

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <SignIn redirectUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL} />
+  );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs';
 export default function Page() {
   return (
     <main className="m-auto p-3">
-      <SignUp />
+      <SignUp redirectUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL} />
     </main>
   );
 }
