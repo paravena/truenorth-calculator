07760dc74db94c1ab41e377777ad020c243d4efc
Pablo Aravena <paravena74@gmail.com>
2023-05-28 13:41:35 -0400

    some debugging

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,12 @@ import { NextResponse } from 'next/server';
 export default authMiddleware({
   afterAuth(auth, req, evt) {
     // handle users who aren't authenticated
+    console.log(
+      `auth.userId: ${auth.userId} isPublicRoute: ${auth.isPublicRoute}`,
+    );
+    console.log(`req.url ${req.url}`);
     if (!auth.userId && !auth.isPublicRoute) {
+      console.log(`Redirecting`);
       const signInUrl = new URL('/sign-in', req.url);
       signInUrl.searchParams.set('redirect_url', req.url);
       return NextResponse.redirect(signInUrl);
