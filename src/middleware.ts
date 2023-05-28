1d013b567b0a6d1b7b574238679c3e31bfadd660
Pablo Aravena <paravena74@gmail.com>
2023-05-28 13:33:28 -0400

    some adjustments

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,16 @@
 import { authMiddleware } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
 
-export default authMiddleware();
+export default authMiddleware({
+  afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      const signInUrl = new URL('/sign-in', req.url);
+      signInUrl.searchParams.set('redirect_url', req.url);
+      return NextResponse.redirect(signInUrl);
+    }
+  },
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
