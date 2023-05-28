eaf933bb8c2a1fb56233629d77e385c61caba996
Pablo Aravena <paravena74@gmail.com>
2023-05-28 14:12:33 -0400

    some debugging

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,14 @@
 import { authMiddleware } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
+const SERVER_URL = process.env.RAILWAY_STATIC_URL || 'http://localhost:3000';
+
 export default authMiddleware({
   afterAuth(auth, req, evt) {
     // handle users who aren't authenticated
+    console.log('RAILWAY_STATIC_URL: ' + process.env.RAILWAY_STATIC_URL);
+    console.log(`SERVER_URL ${SERVER_URL}`);
+
     console.log(
       `auth.userId: ${auth.userId} isPublicRoute: ${auth.isPublicRoute}`,
     );
