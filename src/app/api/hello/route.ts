525fd3cc71ac03946d17460e95c2777ccba5a807
Pablo Aravena <paravena74@gmail.com>
2023-05-28 22:17:00 -0400

    webhooks

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -8,11 +8,19 @@ export async function GET(request: Request) {
 
 export async function POST(request: Request) {
   const data: User = await request.json();
+  console.log('request.body', data, data.emailAddresses);
+
   const id = data.id;
   const email = data.emailAddresses[0].emailAddress;
-  prisma.user.create({
-    data: { email, id: id, authId: id },
-  });
-  console.log('request.body', data);
+
+  console.log(`id ${id} email ${email}`);
+
+  if (id && email) {
+    const newUser = await prisma.user.create({
+      data: { email, id: id, authId: id },
+    });
+    console.log('new user', newUser);
+  }
+
   return new Response('Hello, Next.js!');
 }
