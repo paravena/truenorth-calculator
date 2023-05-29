1f2302e26c32754be3295845a9e83388230f7175
Pablo Aravena <paravena74@gmail.com>
2023-05-28 22:43:48 -0400

    webhooks

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -6,9 +6,9 @@ export async function GET(request: Request) {
 }
 
 export async function POST(request: Request) {
-  const data = await request.json();
+  const { data } = await request.json();
   console.log('data', data);
-  // console.log(`id ${data.id}, email_addresses ${data.email_addresses}`);
+  console.log(`id ${data.id}, email_addresses ${data.email_addresses}`);
 
   // const id = data.id;
   // const email = data.email_addresses[0].email_address;
