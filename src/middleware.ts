e287a3db3e3073ad4c3bd150aef5a4bfbba330bc
Pablo Aravena <paravena74@gmail.com>
2023-06-03 18:49:06 -0400

    upgrading clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -32,7 +32,9 @@ const isPublic = (path: string) => {
 // };
 import { authMiddleware } from '@clerk/nextjs';
 
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ['/sign-in', '/sign-up'],
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
